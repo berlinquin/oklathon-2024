ee608a55f41ee5b448c8f25b4e531a97ac98f825
Quinten Kent <berlinquin@gmail.com>
2024-10-19 15:28:29 -0500

    debug

--- TrafficGram/recordingsession.cpp
+++ TrafficGram/recordingsession.cpp
@@ -18,12 +18,19 @@ void RecordingSession::startRecording()
     QString outputFilename = QString("~/Downloads/out_").append(QString::number(counter)).append(".mp4");
     m_ffmpegProcess->setArguments({"-i", m_trafficCamServer, "-codec", "copy", outputFilename});
     m_ffmpegProcess->start();
+    auto state = m_ffmpegProcess->state();
+
+    connect(m_ffmpegProcess, &QProcess::stateChanged, [&](auto newState){
+        qDebug("newState: %d", newState);
+    });
 }
 
 void RecordingSession::stopRecording()
 {
     auto code = m_ffmpegProcess->exitCode();
     auto status = m_ffmpegProcess->exitStatus();
+    auto output = m_ffmpegProcess->readAllStandardOutput();
+    QString output_str = output;
     m_ffmpegProcess->write("q");
     m_ffmpegProcess->closeWriteChannel();
 }
