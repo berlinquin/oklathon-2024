8fae036a345dddf18a830421b3bb7bc3188d8c31
Quinten Kent <berlinquin@gmail.com>
2024-11-05 20:22:18 -0600

    First work to load camera list from api

--- TrafficGram/CameraListManager.cpp
+++ TrafficGram/CameraListManager.cpp
@@ -1,7 +1,41 @@
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QNetworkRequest>
+#include <QUrl>
+
 #include "CameraListManager.h"
 
 CameraListManager::CameraListManager(QObject *parent)
     : QObject{parent}
 {
+    m_networkManager = new QNetworkAccessManager(this);
+    connect(m_networkManager, &QNetworkAccessManager::finished, this, &CameraListManager::cameraListReceived);
+}
 
+void CameraListManager::loadCameraList()
+{
+    const QUrl cameraPoleEndpoint{"https://oktraffic.org/api/CameraPoles"};
+    QNetworkRequest cameraPoleRequest{cameraPoleEndpoint};
+    m_networkManager->get(cameraPoleRequest);
+}
+
+void CameraListManager::cameraListReceived(QNetworkReply *reply)
+{
+    auto json = QJsonDocument::fromJson(reply->readAll());
+    if (!json.isArray())
+    {
+        emit cameraListReady(QVector<TrafficCamera>{});
+        return;
+    }
+
+    QVector<TrafficCamera> out;
+
+    auto jsonArray = json.array();
+    for (const auto& jsonDoc : jsonArray)
+    {
+        // TODO handle each item in the array
+    }
+    emit cameraListReady(std::move(out));
 }
--- TrafficGram/CameraListManager.h
+++ TrafficGram/CameraListManager.h
@@ -3,13 +3,30 @@
 
 #include <QObject>
 
+class QNetworkAccessManager;
+class QNetworkReply;
+
+struct TrafficCamera
+{
+    QString livestreamUrl;
+};
+
 class CameraListManager : public QObject
 {
     Q_OBJECT
 public:
     explicit CameraListManager(QObject *parent = nullptr);
 
+    void loadCameraList();
+
 signals:
+     void cameraListReady(QVector<TrafficCamera>);
+
+private slots:
+    void cameraListReceived(QNetworkReply *reply);
+
+private:
+    QNetworkAccessManager *m_networkManager;
 
 };
 
