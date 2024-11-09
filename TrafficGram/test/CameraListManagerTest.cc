8878c602bed5ec6d2f668e40cc98f3c05169c7dd
Quinten Kent <berlinquin@gmail.com>
2024-11-09 10:26:01 -0600

    Include source in test cmake

--- TrafficGram/test/CameraListManagerTest.cc
+++ TrafficGram/test/CameraListManagerTest.cc
@@ -9,6 +9,7 @@ TEST(CameraListManagerTest, CameraPoleFromJson) {
     QFile cameraPolesFile("./data/CameraPoles.json");
     ASSERT_TRUE(cameraPolesFile.exists()) << "Rationale: test data should exist";
 
+    cameraPolesFile.open(QIODeviceBase::ReadOnly);
     QByteArray cameraPolesData = cameraPolesFile.readAll();
     auto jsonDocument = QJsonDocument::fromJson(std::move(cameraPolesData));
 
